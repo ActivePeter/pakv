55ba0f6af4c7d5c09777af473be2ced8a6867e1d
ActivePeter <1020401660@qq.com>
2022-07-28 16:23:24 +0800

    done

--- pakv_1/src/main.rs
+++ pakv_1/src/main.rs
@@ -40,7 +40,7 @@ struct Del {
 
 fn main() {
     let cli= Cli::parse();
-
+    cli.
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
